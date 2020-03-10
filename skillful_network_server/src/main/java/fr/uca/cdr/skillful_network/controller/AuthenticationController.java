32832dc7873226cbf3cd8fd3d7a6318af0cf53bc
Anaëlle Alenda <anaelle.alenda@gmail.com>
2020-03-10 17:22:32 +0100

    [AuthentificationController] V1.1 methode findByEmail, cas erreurs, retour user

--- skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/controller/AuthenticationController.java
+++ skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/controller/AuthenticationController.java
@@ -1,28 +1,73 @@
 package fr.uca.cdr.skillful_network.controller;
 
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+import fr.uca.cdr.skillful_network.model.entities.User;
+import fr.uca.cdr.skillful_network.model.repositories.UserRepository;
 
 /**
- * Cette classe a pour rôle d'identifié les utilisateurs.
- * L'authentification des utilisateurs se fait grâce à l'email ou au numéro de mobile (en tant que nom d'utilisateur)
- * ainsi qu'avec un code temporaire envoyé par le serveur à l'email ou au numéro de mobile.
- * Elle est responsable de notamment du traitement des requêtes /login et /token.
+ * Cette classe a pour rôle d'identifié les utilisateurs. L'authentification des
+ * utilisateurs se fait grâce à l'email ou au numéro de mobile (en tant que nom
+ * d'utilisateur) ainsi qu'avec un code temporaire envoyé par le serveur à
+ * l'email ou au numéro de mobile. Elle est responsable de notamment du
+ * traitement des requêtes /login et /token.
  */
 @RestController
 @CrossOrigin(origins = "*")
 public class AuthenticationController {
 
-    /**
-     * @param login the email or the mobile number of the user
-     * @return the id of the user
-     */
-    @RequestMapping(value = "/login", method = POST)
-    public Number login(@RequestBody String login) {
-        return -1;
-    }
+	/**
+	 * @param login the email or the mobile number of the user
+	 * @return the id of the user
+	 */
+
+	@Autowired
+	public UserRepository userRepository;
+
+	@GetMapping(value = "/login")
+	public List<User> login(@RequestBody String login) {
+		return userRepository.findAll();
+	}
+
+	@PostMapping(value = "/login/v1")
+	public ResponseEntity<User> checkpass(@RequestBody User user) throws Exception {
+
+		if (user != null) {
+			User userFromDB = userRepository.findByEmail(user.getEmail())
+					.orElseThrow(() -> new Exception("email not authenticated, user not found: ")); // User object :
+																									// userFromDB
+																									// depending on
+																									// user.getEmail()
+
+			String passwordFromDB = userRepository.findById(userFromDB.getId())
+					.orElseThrow(() -> new Exception("user not found: ")).getPassword(); // get password depending on
+																							// userFromDB
+
+			String passwpordFromInput = user.getPassword(); // get password from user value INPUT
+
+			if (!passwordFromDB.equals(passwpordFromInput)) {
+				System.out.println("pas le même mdp");
+				return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+				
+				// httpStatus.OK
+			}
+			System.out.println("ok");
+			return ResponseEntity.ok().body(userFromDB);
+		}
+		System.out.println("pas user trouvé");
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
 }
--- skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/model/repositories/UserRepository.java
+++ skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/model/repositories/UserRepository.java
@@ -1,11 +1,15 @@
 package fr.uca.cdr.skillful_network.model.repositories;
 
-
 import fr.uca.cdr.skillful_network.model.entities.User;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
+	public Optional<User> findByEmail(String mail);
 }
