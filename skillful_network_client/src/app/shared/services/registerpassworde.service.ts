bb6cecbb3250b44c4c11d609eddc3f25161960d0
Radhwa Marzouk <radhwamarzouk92@gmail.com>
2020-03-10 22:27:19 +0100

    création formulaire et validation des passwords

---
+++ skillful_network_client/src/app/shared/services/registerpassworde.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterpasswordeService {
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+
+
+  registerpassword(formdata){
+    this.http
+      .post("", formdata) 
+      .toPromise() 
+      .then(data => {
+        console.log(data);
+        console.log(data['success']); 
+        if(data['success']){
+          console.log(data); 
+          this.router.navigate(['/home']);
+        } 
+      })
+      .finally(() => {
+      
+      });
+  }
+}
