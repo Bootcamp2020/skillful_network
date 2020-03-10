f3e6057818cceac269bb453439710d102c38c334
Anaëlle Alenda <anaelle.alenda@gmail.com>
2020-03-10 17:32:04 +0100

    [LoginForm.java, AuthenticationController] Ajout de la class LoginForm correspondant à l'authentification d'un utilisateur(mail,password) et utilisation @ResquestBody LoginForm dans le contrôleur

--- skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/controller/AuthenticationController.java
+++ skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/controller/AuthenticationController.java
@@ -1,22 +1,20 @@
 package fr.uca.cdr.skillful_network.controller;
 
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import java.util.Optional;
 
-import java.util.List;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 import fr.uca.cdr.skillful_network.model.entities.User;
 import fr.uca.cdr.skillful_network.model.repositories.UserRepository;
+import fr.uca.cdr.skillful_network.request.LoginForm;
 
 /**
  * Cette classe a pour rôle d'identifié les utilisateurs. L'authentification des
@@ -29,45 +27,25 @@ import fr.uca.cdr.skillful_network.model.repositories.UserRepository;
 @CrossOrigin(origins = "*")
 public class AuthenticationController {
 
-	/**
-	 * @param login the email or the mobile number of the user
-	 * @return the id of the user
-	 */
-
 	@Autowired
 	public UserRepository userRepository;
 
-	@GetMapping(value = "/login")
-	public List<User> login(@RequestBody String login) {
-		return userRepository.findAll();
-	}
-
-	@PostMapping(value = "/login/v1")
-	public ResponseEntity<User> checkpass(@RequestBody User user) throws Exception {
-
-		if (user != null) {
-			User userFromDB = userRepository.findByEmail(user.getEmail())
-					.orElseThrow(() -> new Exception("email not authenticated, user not found: ")); // User object :
-																									// userFromDB
-																									// depending on
-																									// user.getEmail()
-
-			String passwordFromDB = userRepository.findById(userFromDB.getId())
-					.orElseThrow(() -> new Exception("user not found: ")).getPassword(); // get password depending on
-																							// userFromDB
-
-			String passwpordFromInput = user.getPassword(); // get password from user value INPUT
-
-			if (!passwordFromDB.equals(passwpordFromInput)) {
-				System.out.println("pas le même mdp");
-				return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-				
-				// httpStatus.OK
+	@PostMapping(value = "/login")
+	public ResponseEntity<User> authenticateUser(@Valid @RequestBody LoginForm loginRequest) {
+		if (loginRequest != null) {
+			Optional<User> userFromDB = userRepository.findByEmail(loginRequest.getEmail());
+			if (userFromDB.isEmpty()) {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			} else {
+				String passwordFromDB = userFromDB.get().getPassword();
+				String passwordRequest = loginRequest.getPassword();
+				if (passwordRequest != null && !passwordRequest.equals(passwordFromDB)) {
+					return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+				} else {
+					return ResponseEntity.ok().body(userFromDB.get());
+				}
 			}
-			System.out.println("ok");
-			return ResponseEntity.ok().body(userFromDB);
 		}
-		System.out.println("pas user trouvé");
 		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
 }
