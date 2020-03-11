7e577101416ba9a42422535bb7438ca4732ba0c0
BOUNOUA <bounouasihem@gmail.com>
2020-03-11 15:13:58 +0100

    Creation du profil et menu profil

--- skillful_network_client/src/app/app.module.ts
+++ skillful_network_client/src/app/app.module.ts
@@ -21,6 +21,8 @@ import {DashboardComponent} from './home/dashboard/dashboard.component';
 import {UserComponent} from './home/user/user.component';
 import {UsersListComponent} from './home/users-list/users-list.component';
 import {TokenHttpInterceptorService} from './shared/interceptors/token-http-interceptor.service';
+import { ProfileComponent } from './home/dashboard/profile/profile.component';
+import { MenuprofileComponent } from './menuprofile/menuprofile.component';
 
 @NgModule({
     declarations: [ // Chaque composant que vous créez doit être déclaré ici
@@ -29,7 +31,9 @@ import {TokenHttpInterceptorService} from './shared/interceptors/token-http-inte
         LoginComponent,
         DashboardComponent,
         UserComponent,
-        UsersListComponent
+        UsersListComponent,
+        ProfileComponent,
+        MenuprofileComponent
     ],
     imports: [
         BrowserModule,
