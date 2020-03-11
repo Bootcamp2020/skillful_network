c6ec74cf649a8b4317c321389001184b71e54e48
fabrice grisi <fabrice.grisi@laposte.net>
2020-03-11 08:56:02 +0100

    Add CodeGeneration, update User class

---
+++ skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/security/CodeGeneration.java
@@ -0,0 +1,19 @@
+package fr.uca.cdr.skillful_network.security;
+
+public class CodeGeneration {
+
+	public static String generateCode(int length) {
+		// définition des caractères possibles
+		String chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+		//utilisation String builder pour limiter ressources utilisées
+		StringBuilder pass = new StringBuilder();
+		for (int x = 0; x < length; x++) {
+			// tirage au sort d'un caractère et rajout au String builder
+			int i = (int) Math.floor(Math.random() * 62); 
+			pass.append(chars.charAt(i));
+		}
+		System.out.println(pass);
+		return pass.toString();
+	}
+
+}
