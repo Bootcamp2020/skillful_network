3cca1fea1106bcf304f33d5ee18c9ad53cf4e6dc
Anaëlle <anaelle.alenda@gmail.com>
2020-03-17 15:33:57 +0100

    [UserForm, UserController] modification @PutMapping(users/id) en ajoutant d'autres données à mettre à jour et utilisation d'un formUSer en paramètre

--- skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/controller/UserController.java
+++ skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/controller/UserController.java
@@ -4,11 +4,13 @@ import fr.uca.cdr.skillful_network.exceptions.ResourceNotFoundException;
 import fr.uca.cdr.skillful_network.model.entities.User;
 import fr.uca.cdr.skillful_network.model.repositories.UserRepository;
 import fr.uca.cdr.skillful_network.model.services.UserService;
+import fr.uca.cdr.skillful_network.request.UserForm;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.*;
 
@@ -18,38 +20,42 @@ import javax.validation.Valid;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 /**
- * Cette classe est responsable du traitement des requêtes liées aux utilisateurs comme /users.
+ * Cette classe est responsable du traitement des requêtes liées aux
+ * utilisateurs comme /users.
  */
 @RestController
 @CrossOrigin(origins = "*")
 public class UserController {
 
- 	@Autowired
-    private final UserRepository repository;
+	@Autowired
+	private final UserRepository repository;
 	@Autowired
 	private UserService userService;
 
-    public UserController(UserRepository repository) {
-        this.repository = repository;
-    }
+	public UserController(UserRepository repository) {
+		this.repository = repository;
+	}
 
-    @GetMapping(value = "/users")
-    public List<User> getUsers(){
-        return (List<User>) this.repository.findAll();
-    }
-    @Transactional
-    @PutMapping(value="/users/{id}")
-    public ResponseEntity<User> updateUser(@PathVariable(value="id") long id, @Valid @RequestBody User user){
-    	User userToUpdate = userService.getUserById(id);    	
-    	if(userToUpdate == null) {
-    		throw new ResourceNotFoundException("User not found for id " + id);
-    	}    	
-    	userToUpdate.setEmail(user.getEmail());
-    	userToUpdate.setPassword(user.getPassword());
-    	User userUpdated = userService.saveOrUpdateUser(userToUpdate);
-		return new ResponseEntity<User>(userUpdated, HttpStatus.OK);
-    	
-    }
+	@GetMapping(value = "/users")
+	public List<User> getUsers() {
+		return (List<User>) this.repository.findAll();
+	}
 
-
+	@Transactional
+	@PutMapping(value = "/users/{id}")
+	public ResponseEntity<User> updateUser(@PathVariable(value = "id") long id,@Valid @RequestBody UserForm userRequest) {
+		User userToUpdate = userService.getUserById(id);
+		if (userRequest != null) {
+			userToUpdate.setLastName(userRequest.getLastName());
+			userToUpdate.setFirstName(userRequest.getFirstName());
+			userToUpdate.setPassword(userRequest.getPassword());
+			userToUpdate.setBirthDate(userRequest.getBirthDate());
+			userToUpdate.setEmail(userRequest.getEmail());
+			userToUpdate.setMobileNumber(userRequest.getMobileNumber());
+			User userUpdated = userService.saveOrUpdateUser(userToUpdate);
+			return new ResponseEntity<User>(userUpdated, HttpStatus.OK);
+		} else {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Aucune données en paramètre");
+		}
+	}
 }
