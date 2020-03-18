f9311e8e36b10e4131ce81681623dd0cc9631532
Julien Barbera <julien.barbera@gmail.com>
2020-03-18 10:32:47 +0100

    création entité et repo qualification

--- skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/model/entities/Qualification.java
+++ skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/model/entities/Qualification.java
@@ -1,50 +1,72 @@
 package fr.uca.cdr.skillful_network.model.entities;
 
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 @Entity
 public class Qualification {
-	
-	
-	@Id
+
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String qualification;
+    private Long id;
+	@NotNull(message = "Qualification name cannot be null")
+    @Size(min = 2, max = 20, message = "Qualification name must be between 3 and 20 characters")
+    private String name;
 	
-	public String getQualification() {
-		return qualification;
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(name = "qualification_users", joinColumns = { @JoinColumn(name = "qualification_id") }, inverseJoinColumns = {
+			@JoinColumn(name = "user_id") })
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	@JsonIgnore
+	private Set<User> Users = new HashSet<>();
+	
+	
+	public Qualification() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
-	public void setQualification(String qualification) {
-		this.qualification = qualification;
+	public Qualification(long id,
+			@NotNull(message = "Qualification name cannot be null") @Size(min = 2, max = 20, message = "Qualification name must be between 3 and 20 characters") String name) {
+		super();
+		this.id = id;
+		this.name = name;
 	}
-	public Long getId() {
+	public long getId() {
 		return id;
 	}
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
-	public Qualification() {
-		super();
+	public String getName() {
+		return name;
 	}
-	
-	public Qualification(Long id, String qualification) {
-		super();
-		this.id = id;
-		this.qualification = qualification;
+	public void setName(String name) {
+		this.name = name;
 	}
-	
-	public Qualification(String qualification) {
-		super();
-		this.qualification = qualification;
-	}
-	
 	@Override
 	public String toString() {
-		return "Qualification [id=" + id + ", qualification=" + qualification + "]";
+		return "Qualification [id=" + id + ", name=" + name + "]";
 	}
+	
+	
+	
 
 }
--- skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/model/repositories/QualificationRepository.java
+++ skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/model/repositories/QualificationRepository.java
@@ -1,9 +1,12 @@
 package fr.uca.cdr.skillful_network.model.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import fr.uca.cdr.skillful_network.model.entities.Qualification;
 
+
+@Repository
 public interface QualificationRepository extends JpaRepository<Qualification, Long>{
 
 }
