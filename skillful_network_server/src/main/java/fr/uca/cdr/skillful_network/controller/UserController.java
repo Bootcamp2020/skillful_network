b68b7b9043b4bf502e163dd45f2f470402af39d2
Abdelkafi Amal <zaghdanamal@yahoo.fr>
2020-03-18 10:38:52 +0100

    ajout attribut status et photo dans la class user

--- skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/controller/UserController.java
+++ skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/controller/UserController.java
@@ -2,11 +2,17 @@ package fr.uca.cdr.skillful_network.controller;
 
 import fr.uca.cdr.skillful_network.model.entities.User;
 import fr.uca.cdr.skillful_network.model.repositories.UserRepository;
+import fr.uca.cdr.skillful_network.security.CodeGeneration;
+import fr.uca.cdr.skillful_network.security.SendMail;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import javax.validation.Valid;
+
 
 /**
  * Cette classe est responsable du traitement des requêtes liées aux utilisateurs comme /users.
@@ -22,12 +28,14 @@ public class UserController {
 
     public UserController(UserRepository repository) {
         this.repository = repository;
+        
     }
 
     @RequestMapping(value = "/users")
     public List<User> getUsers(){
         return (List<User>) this.repository.findAll();
     }
+    
 
 
 }
