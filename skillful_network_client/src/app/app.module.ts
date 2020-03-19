ae342974a974d0352fa14192e2558156c026eec5
Marc Fernandez <marcf06400@gmail.com>
2020-03-19 14:53:31 +0100

    ajout import MaToolTipModule

--- skillful_network_client/src/app/app.module.ts
+++ skillful_network_client/src/app/app.module.ts
@@ -33,6 +33,7 @@ import { CandidatureComponent } from './home/dashboard/candidature/candidature.c
 
 import { ProfileComponent } from './home/dashboard/profile/profile.component';
 import { MenuprofileComponent } from './menuprofile/menuprofile.component';
+import {MatTooltipModule} from "@angular/material/tooltip";
 
 @NgModule({
     declarations: [ // Chaque composant que vous créez doit être déclaré ici
@@ -65,6 +66,7 @@ import { MenuprofileComponent } from './menuprofile/menuprofile.component';
         MaterialModule, // Ce module que nous avons créé contient l'ensemble des modules graphiques material à utiliser dans le projet
         FlexLayoutModule, // Permet de positionner à l'aide des fxFlex, fxLayout, fxLayoutAlign etc.
         MatFormFieldModule,
+        MatTooltipModule,
     ],
     providers: [
         // Mise en place d'un intercepteur qui permettra d'appliquer le token automatiquement
