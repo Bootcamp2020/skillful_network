c8406cfb2ff4b64c12d2f83b822590ab0888bf40
hanenben <46932184+hanenben@users.noreply.github.com>
2020-03-20 10:55:19 +0100

    Update Application.java

--- skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/Application.java
+++ skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/Application.java
@@ -12,6 +12,8 @@ import fr.uca.cdr.skillful_network.model.entities.JobOffer;
 import fr.uca.cdr.skillful_network.model.entities.Skill;
 import fr.uca.cdr.skillful_network.model.entities.Training;
 import fr.uca.cdr.skillful_network.model.entities.User;
+import fr.uca.cdr.skillful_network.model.entities.Subscription;
+import fr.uca.cdr.skillful_network.model.repositories.SubscriptionRepository;
 import fr.uca.cdr.skillful_network.model.repositories.JobOfferRepository;
 import fr.uca.cdr.skillful_network.model.repositories.SkillRepository;
 import fr.uca.cdr.skillful_network.model.repositories.TrainingRepository;
@@ -62,6 +64,16 @@ public class Application {
 		return args -> {
 			if (SkillRepository.findAll().isEmpty()) {
 				new JSONLoader<>("src/main/resources/data/skills.json", Skill[].class, SkillRepository).load();
+			}
+		};
+	}
+	@Bean
+	ApplicationRunner initSubscriptionRepository(SubscriptionRepository subscriptionRepository) {
+		return args -> {
+			if (subscriptionRepository.findAll().isEmpty()) {
+				new JSONLoader<>("src/main/resources/data/subscriptions.json", Subscription[].class, subscriptionRepository)
+						.load();
+				
 			}
 		};
 	}
