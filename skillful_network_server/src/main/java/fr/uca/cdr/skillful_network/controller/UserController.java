9643941e44369c7a9d8b905189ea70d7eedbf2e9
Anaëlle <anaelle.alenda@gmail.com>
2020-03-20 12:27:06 +0100

    AA_2.3.4 v.1 [UserController,UserForm] Modification afin de mettre à jour la liste d'abonnements de l'utilisateur @PutMapping(/user/id)

--- skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/controller/UserController.java
+++ skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/controller/UserController.java
@@ -71,6 +71,7 @@ public class UserController {
 				userToUpdate.setMobileNumber(userRequest.getMobileNumber());
 				userToUpdate.setSkillSet(userRequest.getSkillSet());
 				userToUpdate.setQualificationSet(userRequest.getQualificationSet());
+				userToUpdate.setSubscriptionSet(userRequest.getSubscriptionSet());
 				User userUpdated = userService.saveOrUpdateUser(userToUpdate);
 				return new ResponseEntity<User>(userUpdated, HttpStatus.OK);
 			} else {
