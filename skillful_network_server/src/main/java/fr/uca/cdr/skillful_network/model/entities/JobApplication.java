383d0c3a6d7072efbf30eea562461c4ad840693a
Philou <pfouque@gmail.com>
2020-03-23 16:39:37 +0100

    PF_12.3 init @Entity JobApplication.java class

---
+++ skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/model/entities/JobApplication.java
@@ -0,0 +1,110 @@
+package fr.uca.cdr.skillful_network.model.entities;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "jobApplications")
+public class JobApplication {
+
+    enum ApplicationStatus { INIT, SUBMITTED, INVESTIGATING, WAITING, PAUSED, POSTPONED, ACCEPTED, REJECTED}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+//    @ManyToOne(cascade = CascadeType.ALL)
+//    @JoinColumn(name = "user_id")
+    private User user;
+//    @ManyToOne(cascade = CascadeType.ALL)
+//    @JoinColumn(name = "joboffer_id")
+    private JobOffer jobOffer;
+    private ApplicationStatus status;
+    private Date submitDate;
+    private Date AcceptDate;
+
+    public JobApplication() { super(); }
+
+    public JobApplication(User user, JobOffer jobOffer, ApplicationStatus status, Date submitDate, Date acceptDate) {
+        super();
+        this.user = user;
+        this.jobOffer = jobOffer;
+        this.status = status;
+        this.submitDate = submitDate;
+        AcceptDate = acceptDate;
+    }
+
+    public JobApplication(Long id, User user, JobOffer jobOffer, ApplicationStatus status, Date submitDate, Date acceptDate) {
+        super();
+        this.id = id;
+        this.user = user;
+        this.jobOffer = jobOffer;
+        this.status = status;
+        this.submitDate = submitDate;
+        AcceptDate = acceptDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public JobOffer getJobOffer() {
+        return jobOffer;
+    }
+
+    public void setJobOffer(JobOffer jobOffer) {
+        this.jobOffer = jobOffer;
+    }
+
+    public ApplicationStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ApplicationStatus status) {
+        this.status = status;
+    }
+
+    public Date getSubmitDate() {
+        return submitDate;
+    }
+
+    public void setSubmitDate(Date submitDate) {
+        this.submitDate = submitDate;
+    }
+
+    public Date getAcceptDate() {
+        return AcceptDate;
+    }
+
+    public void setAcceptDate(Date acceptDate) {
+        AcceptDate = acceptDate;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, user, jobOffer);
+    }
+
+    @Override
+    public String toString() {
+        return "JobApplication{" +
+                "id=" + id +
+                ", user=" + user.getFirstName() + " " + user.getLastName() +
+                ", jobOffer=" + jobOffer.getCompany() + "/" + jobOffer.getName() +
+                ", status=" + status +
+                '}';
+    }
+}
