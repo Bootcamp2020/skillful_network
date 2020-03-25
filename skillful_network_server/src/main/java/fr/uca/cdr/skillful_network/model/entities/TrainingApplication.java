c130e99a8727e105a365fc04a0c8d05df2d3ce4f
Danglot <bdanglot@gmail.com>
2020-03-25 23:31:20 +0100

    BD_12.1 refactor Applications to avoid redundancy

--- skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/model/entities/TrainingApplication.java
+++ skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/model/entities/TrainingApplication.java
@@ -8,24 +8,12 @@ import java.util.Objects;
 
 @Entity
 @Table(name = "trainingApplications")
-public class TrainingApplication {
+public class TrainingApplication extends Application {
 
-    enum ApplicationStatus {INIT, SUBMITTED, INVESTIGATING, WAITING, PAUSED, POSTPONED, ACCEPTED, REJECTED}
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @ManyToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "user_id")
-    @JsonIgnore
-    private User user;
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "training_id")
     @JsonIgnore
     private Training training;
-    private ApplicationStatus status;
-    private Date submitDate;
 
     public TrainingApplication() {
         super();
@@ -42,32 +30,13 @@ public class TrainingApplication {
     }
 
     public TrainingApplication(User user, Training training, ApplicationStatus status, Date submitDate) {
-        super();
-        this.user = user;
+        super(user, status, submitDate);
         this.training = training;
-        this.status = status;
-        this.submitDate = submitDate;
     }
 
     public TrainingApplication(Long id, User user, Training training, ApplicationStatus status, Date submitDate) {
-        this(user, training, status, submitDate);
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
+        super(id, user, status, submitDate);
+        this.training = training;
     }
 
     public Training getTraining() {
@@ -76,22 +45,6 @@ public class TrainingApplication {
 
     public void setTraining(Training training) {
         this.training = training;
-    }
-
-    public ApplicationStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(ApplicationStatus status) {
-        this.status = status;
-    }
-
-    public Date getSubmitDate() {
-        return submitDate;
-    }
-
-    public void setSubmitDate(Date submitDate) {
-        this.submitDate = submitDate;
     }
 
     @Override
