47a8a63433c3a163b1c093b23058a52827c00ec4
Valerie Baron <valerie.baron67@gmail.com>
2020-03-27 17:07:32 +0100

    VB_correction bug lazy=>eager dans user entity pour les roles

--- skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/model/entities/User.java
+++ skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/model/entities/User.java
@@ -48,7 +48,7 @@ public class User {
 	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
 	private Set<TrainingApplication> trainingApplicationSet = new HashSet<>();
 	
-	@ManyToMany(fetch = FetchType.LAZY)
+	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
 
