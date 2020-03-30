4635ffa161a63dcc80a4498349679748bfd122de
Philou <pfouque@gmail.com>
2020-03-30 14:10:23 +0200

    fix missing import

--- skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/controller/TrainingApplicationController.java
+++ skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/controller/TrainingApplicationController.java
@@ -7,6 +7,7 @@ import fr.uca.cdr.skillful_network.model.services.TrainingApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
