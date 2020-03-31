97acd9463bdc06f377b229f2ebb5a1641ba7762c
Anaëlle <anaelle.alenda@gmail.com>
2020-03-31 16:08:15 +0200

    AA_3.R.1.1B v1.0 create pagingand sorting sytem and some requests GET

--- skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/model/repositories/JobOfferRepository.java
+++ skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/model/repositories/JobOfferRepository.java
@@ -1,9 +1,13 @@
 package fr.uca.cdr.skillful_network.model.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import fr.uca.cdr.skillful_network.model.entities.JobOffer;
 
-public interface JobOfferRepository extends JpaRepository<JobOffer, Long>{
+public interface JobOfferRepository extends JpaRepository<JobOffer, Long> {
+
+	Page<JobOffer> findByNameOrCompanyContaining(Pageable pageable, String keyword1, String keyword2);
 
 }
