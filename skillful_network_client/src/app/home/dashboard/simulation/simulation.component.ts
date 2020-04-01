ebf3a0594cb115bc0a446ce5408b51d33027c523
Julienb23 <60656019+Julienb23@users.noreply.github.com>
2020-04-01 16:19:39 +0200

    JB_22.2 v1.0 [simulation.component, mock.simulation] affichage de la liste (#73)
    * JB_22.2 v1.0 [simulation.component, mock.simulation] affichage liste des simulations passées dans le dashboard.
    
    * Update simulation.component.html
    
    * Update simulation.component.ts
    
    * Update simulation.ts

--- skillful_network_client/src/app/home/dashboard/simulation/simulation.component.ts
+++ skillful_network_client/src/app/home/dashboard/simulation/simulation.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { MOCK_SIMULATION, IPost } from 'src/app/shared/models/mock.simulation';
+import { Simulation } from 'src/app/shared/models/simulation';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatSort} from '@angular/material/sort';
+
+const ELEMENT_DATA: IPost[] =MOCK_SIMULATION;
 
 @Component({
   selector: 'app-simulation',
@@ -6,9 +12,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./simulation.component.scss']
 })
 export class SimulationComponent implements OnInit {
-  constructor() { }
-
-  ngOnInit(): void {
+  displayedColumns: string[] = ['nbexercice', 'date', 'jobgoal', 'simdetail', 'reloadsim'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  public listSimulation: Simulation[];
+  @Input() public nbexercice: number;
+  @Input() public date: Date;
+  @Input() public caeerGoal: String;
+ 
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  
+  constructor() {
   }
 
+  ngOnInit(): void {
+    this.listSimulation = [];
+    MOCK_SIMULATION.forEach((simulation: IPost) => {
+      this.listSimulation.push(new Simulation(simulation));    
+      this.dataSource.sort = this.sort;
+    });  
+  }   
 }
