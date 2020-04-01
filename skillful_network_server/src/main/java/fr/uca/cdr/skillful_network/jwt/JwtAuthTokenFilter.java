830b6c2e5d7378d0b73163e576915ab6ec425336
Samuel Huguet <samuel.huguet01@gmail.com>
2020-04-01 10:30:42 +0200

    SH_VZ_1.20.6 [JwtAuthEntryPoint, JwtAuthTokenFilter] Ajout des filtres d'authentification relatifs au token

---
+++ skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/jwt/JwtAuthTokenFilter.java
@@ -0,0 +1,63 @@
+package fr.uca.cdr.skillful_network.jwt;
+
+import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import fr.uca.cdr.skillful_network.security.services.UserDetailsServiceImpl;
+
+@Component
+public class JwtAuthTokenFilter extends OncePerRequestFilter {
+	@Autowired
+	private JwtProvider tokenProvider;
+	@Autowired
+	private UserDetailsServiceImpl userDetailsService;
+
+	private static final Logger logger = LoggerFactory.getLogger(JwtAuthTokenFilter.class);
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		try {
+			String jwt = getJwt(request);
+			String decriptResponse = tokenProvider.decryptJwtToken(jwt);
+			if (jwt != null && tokenProvider.validateToken(decriptResponse)) {
+				String email = tokenProvider.getEmailFromToken(decriptResponse);
+
+				UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+				UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+						userDetails, null, userDetails.getAuthorities());
+				authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+				SecurityContextHolder.getContext().setAuthentication(authentication);
+			}
+		} catch (Exception e) {
+			logger.error("Can NOT set user authentication -> Message: {}", e);
+		}
+
+		filterChain.doFilter(request, response);
+	}
+
+	private String getJwt(HttpServletRequest request) {
+		String authHeader = request.getHeader("Authorization");
+
+		if (authHeader != null && authHeader.startsWith("Bearer ")) {
+			return authHeader.replace("Bearer ", "");
+		}
+
+		return null;
+	}
+}
