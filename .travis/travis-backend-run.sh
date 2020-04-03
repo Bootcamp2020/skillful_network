4c4506fca72092421515014cfb585996087a0fc0
Danglot <bdanglot@gmail.com>
2020-04-03 11:18:32 +0200

    ci: add a new job on Travis to exec the server

---
+++ .travis/travis-backend-run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+mvn clean package spring-boot:run -DskipTests   -f skillful_network_server/pom.xml
---
+++ .travis/travis-backend-test.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+mvn clean install -f skillful_network_server/pom.xml
