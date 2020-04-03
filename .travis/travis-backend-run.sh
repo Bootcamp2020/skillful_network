1c34aa6131add91dd1a964ce876e298fc851f973
Danglot <bdanglot@gmail.com>
2020-04-03 11:49:14 +0200

    ci: use now the test profile

--- .travis/travis-backend-run.sh
+++ .travis/travis-backend-run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-mvn clean package spring-boot:run -DskipTests   -f skillful_network_server/pom.xml
+mvn clean package spring-boot:run -DskipTests -Dspring-boot.run.profiles=test -f skillful_network_server/pom.xml
