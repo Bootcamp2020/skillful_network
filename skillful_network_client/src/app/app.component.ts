a63c45ec2c0d7a9e7ac7cde44930dbc238b9acde
YounesTmr <temardriller@outlook.fr>
2020-04-16 11:28:43 +0200

    Prevent connected user to go to login , password and passwordForgotten / redirect to home page with a snackBar message

--- skillful_network_client/src/app/app.component.ts
+++ skillful_network_client/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { filter } from 'rxjs/operators';
 import { pipe } from 'rxjs';
 import { ApiHelperService } from './shared/services/api-helper.service';
 import { UserService } from './shared/services/user.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
@@ -15,10 +16,11 @@ export class AppComponent implements OnInit{
   public stillLogged:Boolean;
   public isLoading :Boolean;
   public urlActual : String ;
-  constructor(private tokenStorageService: TokenStorageService, private router: Router , private api:ApiHelperService , private userService : UserService) {
+  constructor(private tokenStorageService: TokenStorageService, private snackBar: MatSnackBar,private router: Router , private api:ApiHelperService , private userService : UserService) {
     
   }
   ngOnInit(): void {  
+        
         this.urlActual = location.pathname.toString()
         if(!(this.urlActual.includes("login") || this.urlActual.includes("password") || this.urlActual.includes("passwordForgotten"))){
           this.stillLogged = true
@@ -31,11 +33,39 @@ export class AppComponent implements OnInit{
       .catch(err=>{
               localStorage.clear()
               this.router.navigate(['/login']);
+              this.snackBar.open("Votre token a été expiré, veuillez vous connectez de nouveau !", "", {
+                duration: 5000,
+             });
               this.stillLogged = false
             })
+        
         }
+        else {
+          if(this.tokenStorageService.getToken()!= null){
+            this.stillLogged = true
+            this.api.post({endpoint:"/authentication/whoami" , data:this.tokenStorageService.getToken()}).then(
+              data=>{
+                this.userService.updateUser(data,false);
+                this.router.navigate(['/home']);
+                this.snackBar.open("Vous êtes déja connecté, veuillez vous déconnecter pour vous connecter à un autre compte !", "", {
+                  duration: 5000,
+               });
+                this.stillLogged = false
+              }
+              )
+        .catch(err=>{
+                localStorage.clear()
+                this.router.navigate(['/login']);
+                this.stillLogged = false
+              })
+          }
+        }
+
+        
+         
+        
     
   }
-  
+
 }
 
